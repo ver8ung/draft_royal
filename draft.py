407981a5b6148b992c2496f327ebf1b1b2d23aa4
ver8ung <45457029+ver8ung@users.noreply.github.com>
2019-08-06 22:10:49 +0200

    Update draft.py

--- draft.py
+++ draft.py
@@ -1,6 +1,12 @@
 import os
 from PIL import Image
 import glob
+#import matplotlib.pyplot as plt 
+#import matplotlib.image as img 
 
 os.chdir('C:\Users\maxru_000\Desktop\clash_images')
 card_images_list = sorted(glob.glob('C:\Users\maxru_000\Desktop\clash_images'))
+images_to_merge = random.choice(card_images_list, k=32)
+
+#im = img.imread(images_to_merge)
+#plt.imshow(im)
